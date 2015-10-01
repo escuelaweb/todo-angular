6577f14cd144dbbfc1636173d514715d85773945
Sergio Marin <higher.vnf@gmail.com>
2015-10-01 14:28:50 -0300

    Change all gulpfile to precompile angular partials

--- gulpfile.js
+++ gulpfile.js
@@ -9,6 +9,10 @@ var gulp = require('gulp'),
     uuid = require('node-uuid'),
     ngAnnotate = require('gulp-ng-annotate'),
     rename = require("gulp-rename"),
+    concat = require("gulp-concat"),
+    ngHtml2Js = require("gulp-ng-html2js"),
+    inject = require('gulp-inject'),
+    es = require('event-stream'),
     sass = require('gulp-sass');
 
 var source_paths = {
@@ -16,40 +20,105 @@ var source_paths = {
   js: './source/js/app.js',
   all_js: './source/js/**/*.js',
   html: './source/html/**/*.html',
+  html_index: './source/html/index.html',
+  partials: './source/html/views/**/*.html',
+  partials_dest: './source/js/partials',
   dev_css: './build/css',
   dev_js: './build/js',
   dev_html: './build/',
   prod_css: './dist/css',
   prod_js: './dist/js',
-  prod_html: './dist'
+  prod_html: './dist',
+  prod_partials: './dist/views',
+  inject_dev: ['./build/js/app.js', './build/css/app.css'],
+  inject_prod: [],  
 }
 
-gulp.task('sass', function() {
-  gulp.src(source_paths.sass)
-    .pipe(sass.sync().on('error', sass.logError))
-    .pipe(gulp.dest(source_paths.dev_css));
-})
-
-gulp.task('copy', function() {
-  gulp.src(source_paths.html)
-    .pipe(gulp.dest(source_paths.dev_html));
-})
-
-gulp.task('browserify', function() {
-  browserify([source_paths.js], {
+tasks = {
+  baseBrowserify: function() {
+   return browserify([source_paths.js], {
           transform: ['babelify']
       })
       .bundle()
       .pipe(source('app.js'))
+  },
+  prodBrowserify: function() {
+    return this.baseBrowserify()
+      .pipe(ngAnnotate())
+      .pipe(uglify())
+      .pipe(rename(tasks.assetProdName('js')))
+      .pipe(gulp.dest(source_paths.prod_js))
+  },
+  devBrowserify: function() {
+    return this.baseBrowserify()
       .pipe(gulp.dest(source_paths.dev_js));
+  },
+  prodCss: function() {
+    return gulp.src(source_paths.sass)
+      .pipe(sass.sync().on('error', sass.logError))
+      .pipe(minifyCss({compatibility: 'ie8'}))
+      .pipe(rename(tasks.assetProdName('css')))
+      .pipe(gulp.dest(source_paths.prod_css));
+  },
+  devCss: function() {
+    return gulp.src(source_paths.sass)
+      .pipe(sass.sync().on('error', sass.logError))
+      .pipe(gulp.dest(source_paths.dev_css));
+  },
+  injectHtml: function(dest, injected_files) {
+    return gulp.src(source_paths.html_index)
+      .pipe(inject(injected_files, 
+                  {
+                    ignorePath: ['dist', 'build', 'source'],
+                    removeTags: true,
+                  }))
+      .pipe(gulp.dest(dest))
+  },
+  BaseNgHtml: function(dest) {
+    return gulp.src(source_paths.partials)
+      .pipe(htmlmin({
+        empty: true,
+        spare: true,
+        quotes: true
+      }))
+      .pipe(ngHtml2Js({
+        moduleName: "PartialsPrecompile",
+        prefix: "./views/"
+      }))
+      .pipe(concat("all.js"))
+      .pipe(uglify())
+      .pipe(gulp.dest(dest))
+  },
+  assetProdName: function(type) {
+    var name = "app-" + uuid.v1() + "." + type; 
+    source_paths.inject_prod.push("./dist/" + type + "/" + name)
+    console.log(source_paths.inject_prod)
+    return name
+  },
+}
+
+gulp.task('ngHtml', function() {
+  tasks.BaseNgHtml(source_paths.partials_dest)
 })
 
-gulp.task('build', ['sass','browserify','copy'])
+gulp.task('inject', ['ngHtml'], function() {
+  tasks.injectHtml(source_paths.dev_html, es.merge(tasks.devCss(), tasks.devBrowserify()))  
+})
+
+gulp.task('inject:prod',['ngHtml'], function() {
+  tasks.injectHtml(source_paths.prod_html, es.merge(tasks.prodCss(), tasks.prodBrowserify()))
+});
+
+gulp.task('browserify', function() {
+  tasks.devBrowserify()
+})
+
+gulp.task('build', ['inject'])
 
 gulp.task('build:watch', ['build'], function() {
   gulp.watch(source_paths.sass, ['sass'])
   gulp.watch(source_paths.all_js, ['browserify'])
-  gulp.watch(source_paths.html, ['copy'])
+  gulp.watch(source_paths.html, ['inject'])
 });
 
 gulp.task('serve', ['build:watch'],function() {
@@ -57,28 +126,6 @@ gulp.task('serve', ['build:watch'],function() {
     .pipe(webserver({open: true, livereload: true}));
 });
 
-gulp.task('sass:prod', ['sass'], function() {
-  gulp.src(source_paths.dev_css + '/app.css')
-    .pipe(minifyCss({compatibility: 'ie8'}))
-    //.pipe(rename('app-' + uuid.v1() + '.css'))
-    .pipe(gulp.dest(source_paths.prod_css));
-});
-
-gulp.task('browserify:prod', ['browserify'], function() {
-  gulp.src(source_paths.dev_js + '/app.js')
-    .pipe(ngAnnotate())
-    .pipe(uglify())
-    //.pipe(rename('app-' + uuid.v1() + '.js'))
-    .pipe(gulp.dest(source_paths.prod_js));
-});
-
-gulp.task('html:prod', ['copy'], function() {
-  gulp.src(source_paths.dev_html + '/**/*.html')
-    .pipe(htmlmin({collapseWhitespace: true, removeComments: true}))
-    .pipe(gulp.dest(source_paths.prod_html));
-})
-
-
-gulp.task('dist', ['sass:prod','browserify:prod', 'html:prod']);
+gulp.task('dist', ['inject:prod']);
 
 gulp.task('default', ['build']);
--- source/js/app.js
+++ source/js/app.js
@@ -2,13 +2,13 @@ import angular from 'angular'
 import services from './services/all'
 import controllers from './controllers/all'
 import directives from './directives/all'
-
+import partials from './partials/all'
 // Se define un nuevo modulo de angular, para definir el modulo
 // el mismo debe tener un segundo parametro con dependencias, en caso
 // de no tener dependencias debe pasarse un array vacio.
 // Esto debido a que el .module(params1, params2) con dos parametros
 // es el set crear un nuevo modulo y con un solo parametro es un get
-angular.module('TodoApp', ['TodoApp.directives','TodoApp.controllers'])
+angular.module('TodoApp', ['TodoApp.directives','TodoApp.controllers', 'PartialsPrecompile'])
 
 // El metodo run de un modulo, nos permite ejecutar codigo al momento que se
 // monta el Angular por medio de la directiva ng-app
